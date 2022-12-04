2b83343135e4a82e2239d24fb6d8077d34ee8ddd
Ali_Sdg90 <115698554+Ali-Sdg90@users.noreply.github.com>
2022-12-04 14:57:07 +0330

    change theme method

--- JS/script.js
+++ JS/script.js
@@ -1,120 +1,82 @@
 const themeBtn = document.getElementById("change-theme");
 const localTheme = localStorage.getItem("localTheme");
 let themeCounter = 1;
-
+const root = document.querySelector(":root");
 if (localTheme) themeCounter = localTheme;
 changeTheme();
 function changeTheme() {
     // console.log(themeCounter);
-    document.documentElement.style.setProperty("--transition", "0.5s");
     //black ⇩
     if (themeCounter == 0) {
-        document.documentElement.style.setProperty(
-            "--background",
-            "rgb(36, 36, 40)"
-        );
-        document.documentElement.style.setProperty("--main-color", "white");
-        document.documentElement.style.setProperty(
-            "--line-color",
-            "rgb(65, 115, 208)"
-        );
-        document.documentElement.style.setProperty(
-            "--light-key-color",
-            "rgb(80, 79, 86)"
-        );
-        document.documentElement.style.setProperty(
-            "--dark-key-color",
-            "rgb(65, 64, 71)"
-        );
-        document.documentElement.style.setProperty(
-            "--btn-hover",
-            "rgba(84, 84, 84, 0.73)"
-        );
-        document.documentElement.style.setProperty(
-            "--btn-active",
-            "rgb(47, 46, 51)"
-        );
-        document.documentElement.style.setProperty(
-            "--key-hover",
-            "rgb(52, 51, 56)"
-        );
-        document.documentElement.style.setProperty("--radius-keys", "2px");
-        document.documentElement.style.setProperty("--radius-btns", "4px");
-        document.documentElement.style.setProperty(
-            "--font-family",
-            "sans-serif"
-        );
-        document.documentElement.style.setProperty("--font-style", "normal");
+        root.style.cssText = `
+            --font-family: sans-serif;
+            --font-style: normal;
+            --transition: 0.5s;
+            --radius-keys: 2px;
+            --radius-btns: 4px;
+            --main-color: white;
+            --background: rgb(36, 36, 40);
+            --line-color: rgb(65, 115, 208);
+            --light-key-color: rgb(80, 79, 86);
+            --dark-key-color: rgb(65, 64, 71);
+            --btn-hover: rgba(84, 84, 84, 0.73);
+            --btn-active: rgb(47, 46, 51);
+            --key-hover: rgb(52, 51, 56);
+            `;
     }
     //white ⇩
     if (themeCounter == 1) {
-        document.documentElement.style.setProperty("--main-color", "black");
-        document.documentElement.style.setProperty(
-            "--background",
-            "rgb(203, 203, 203)"
-        );
-        document.documentElement.style.setProperty(
-            "--line-color",
-            "rgb(208, 134, 65)"
-        );
-        document.documentElement.style.setProperty(
-            "--light-key-color",
-            "rgb(241, 241, 241)"
-        );
-        document.documentElement.style.setProperty(
-            "--dark-key-color",
-            "rgb(228, 228, 228)"
-        );
-        document.documentElement.style.setProperty(
-            "--btn-hover",
-            "rgb(188, 188, 188)"
-        );
-        document.documentElement.style.setProperty(
-            "--btn-active",
-            "rgb(191, 191, 191)"
-        );
-        document.documentElement.style.setProperty("--key-hover", "#c8c8c8");
+        root.style.cssText = `
+            --font-family: sans-serif;
+            --font-style: normal;
+            --transition: 0.5s;
+            --radius-keys: 2px;
+            --radius-btns: 4px;
+            --main-color: black;
+            --background: rgb(203, 203, 203);
+            --line-color: rgb(208, 134, 65);
+            --light-key-color: rgb(241, 241, 241);
+            --dark-key-color: rgb(228, 228, 228);
+            --btn-hover: rgb(188, 188, 188);
+            --btn-active: rgb(191, 191, 191);
+            --key-hover: #c8c8c8;
+        `;
     }
     //green ⇩
     if (themeCounter == 2) {
-        document.documentElement.style.setProperty("--background", "#D6EE73");
-        document.documentElement.style.setProperty("--line-color", "#D2C250");
-        document.documentElement.style.setProperty(
-            "--light-key-color",
-            "#98D42A"
-        );
-        document.documentElement.style.setProperty(
-            "--dark-key-color",
-            "#72D429"
-        );
-        document.documentElement.style.setProperty("--btn-hover", "#36A533");
-        document.documentElement.style.setProperty("--key-hover", "#36A533");
-        document.documentElement.style.setProperty("--btn-active", "#2e802c");
-        document.documentElement.style.setProperty("--radius-keys", "16px");
-        document.documentElement.style.setProperty("--radius-btns", "20px");
-        document.documentElement.style.setProperty(
-            "--font-family",
-            "monospace"
-        );
+        root.style.cssText = `
+            --font-family: monospace;
+            --font-style: normal;
+            --transition: 0.5s;
+            --radius-keys: 16px;
+            --radius-btns: 20px;
+            --main-color: black;
+            --background: #D6EE73;
+            --line-color: #D2C250;
+            --light-key-color: #98D42A;
+            --dark-key-color: #72D429;
+            --btn-hover: #36A533;
+            --btn-active: rgb(54, 165, 51);
+            --key-hover: #36A533;
+        `;
     }
     //blue ⇩
     if (themeCounter == 3) {
-        document.documentElement.style.setProperty("--background", "#C0ECFB");
-        document.documentElement.style.setProperty(
-            "--light-key-color",
-            "#73D6F3"
-        );
-        document.documentElement.style.setProperty(
-            "--dark-key-color",
-            "#1AC8F2"
-        );
-        document.documentElement.style.setProperty("--btn-hover", "#006CBF");
-        document.documentElement.style.setProperty("--key-hover", "#006DC1");
-        document.documentElement.style.setProperty("--btn-active", "#0060a5");
-        document.documentElement.style.setProperty("--radius-keys", "15px 0");
-        document.documentElement.style.setProperty("--radius-btns", "10px");
-        document.documentElement.style.setProperty("--font-family", "cursive");
-        document.documentElement.style.setProperty("--font-style", "italic");
+        root.style.cssText = `
+            --font-family: cursive;
+            --font-style: italic;
+            --transition: 0.5s;
+            --radius-keys: 15px 0;
+            --radius-btns: 10px;
+            --main-color: black;
+            --background: #C0ECFB;
+            --line-color: #D2C250;
+            --light-key-color: #73D6F3;
+            --dark-key-color: #1AC8F2;
+            --btn-hover: #006CBF;
+            --btn-active: #006CBF;
+            --key-hover: #006DC1;
+        `;
     }
     setTimeout(() => {
         document.documentElement.style.setProperty(
