f407aa4e5d3ef7e9b20c277c9540ec7601d43c65
Ali_Sdg90 <115698554+Ali-Sdg90@users.noreply.github.com>
2023-02-04 22:58:17 +0330

    Add theme scsss / add transitionEffect

--- js/script.js
+++ js/script.js
@@ -1,91 +1,41 @@
 const themeBtn = document.getElementById("change-theme");
 const localTheme = localStorage.getItem("localTheme");
-const root = document.querySelector(":root");
+const transitionCircle = document.getElementById("transition-circle");
 let themeCounter = 0;
 
-if (localTheme) themeCounter = localTheme;
+localStorage.clear();
+
+if (localTheme) themeCounter = Number(localTheme);
 changeTheme();
 function changeTheme() {
     switch (themeCounter) {
         //black ⇩
         case 0:
-            root.style.cssText = `
-                --font-family: sans-serif;
-                --font-style: normal;
-                --transition: 0.5s;
-                --radius-keys: 2px;
-                --radius-btns: 4px;
-                --main-color: white;
-                --background: rgb(36, 36, 40);
-                --line-color: rgb(65, 115, 208);
-                --light-key-color: rgb(80, 79, 86);
-                --dark-key-color: rgb(65, 64, 71);
-                --btn-hover: rgba(84, 84, 84, 0.73);
-                --active: rgb(47, 46, 51);
-                --key-hover: rgb(52, 51, 56);
-                `;
+            // transitionEffect("./css/theme-black.css");
+            document.getElementById("theme-stylesheet").href = "./css/theme-black.css";
             break;
         //white ⇩
         case 1:
-            root.style.cssText = `
-                --font-family: sans-serif;
-                --font-style: normal;
-                --transition: 0.5s;
-                --radius-keys: 2px;
-                --radius-btns: 4px;
-                --main-color: black;
-                --background: rgb(203, 203, 203);
-                --line-color: rgb(208, 134, 65);
-                --light-key-color: rgb(241, 241, 241);
-                --dark-key-color: rgb(228, 228, 228);
-                --btn-hover: rgb(188, 188, 188);
-                --active: rgb(191, 191, 191);
-                --key-hover: #c8c8c8;
-                `;
+            // transitionEffect("./css/theme-white.css");
+            document.getElementById("theme-stylesheet").href = "./css/theme-white.css"
             break;
         //green ⇩
         case 2:
-            root.style.cssText = `
-                --font-family: monospace;
-                --font-style: normal;
-                --transition: 0.5s;
-                --radius-keys: 16px;
-                --radius-btns: 20px;
-                --main-color: black;
-                --background: #D6EE73;
-                --line-color: #D2C250;
-                --light-key-color: #98D42A;
-                --dark-key-color: #72D429;
-                --btn-hover: #36A533;
-                --active: rgb(54, 165, 51);
-                --key-hover: #36A533;
-                `;
+            // transitionEffect("./css/theme-green.css");
+            document.getElementById("theme-stylesheet").href = "./css/theme-green.css"
             break;
         //blue ⇩
         case 3:
-            root.style.cssText = `
-                --font-family: cursive;
-                --font-style: italic;
-                --transition: 0.5s;
-                --radius-keys: 15px 0;
-                --radius-btns: 10px;
-                --main-color: black;
-                --background: #C0ECFB;
-                --line-color: #D2C250;
-                --light-key-color: #73D6F3;
-                --dark-key-color: #1AC8F2;
-                --btn-hover: #006CBF;
-                --active: #006CBF;
-                --key-hover: #006DC1;
-                `;
+            // transitionEffect("./css/theme-blue.css");
+            document.getElementById("theme-stylesheet").href = "./css/theme-blue.css"
             break;
     }
-    setTimeout(() => {
-        document.documentElement.style.setProperty(
-            "--transition",
-            "color 0.15s, background-color 0.15s"
-        );
-    }, 500);
+    // setTimeout(() => {
+    //     document.documentElement.style.setProperty(
+    //         "--transition",
+    //         "color 0.15s, background-color 0.15s"
+    //     );
+    // }, 500);
     localStorage.setItem("localTheme", themeCounter);
 }
 themeBtn.addEventListener("click", function () {
@@ -95,8 +45,20 @@ themeBtn.addEventListener("click", function () {
     } else themeCounter++;
     changeTheme();
 });
-// localStorage.clear();
 
+function transitionEffect(path) {
+    transitionCircle.style.display = "block";
+    setTimeout(() => {
+        transitionCircle.style.outlineWidth = "2000px";
+    }, 1);
+    setTimeout(() => {
+        transitionCircle.style.outlineWidth = "0px";
+        document.getElementById("theme-stylesheet").href = path;
+    }, 480);
+    setTimeout(() => {
+        transitionCircle.style.display = "none";
+    }, 990);
+}
 //---------------------
 
 //Calculator calculation operations :
